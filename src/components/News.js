5b4d969ed4948b1a54a0c21a781a1d81d24a281c
zeeshan0987 <zeeshan0987ali@gmail.com>
2023-08-08 17:27:08 +0530

    Adding Infinite Scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,26 +2,26 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
-  static defaultProps={
-    country:'in',
-    pageSize:9,
-    category:"science",
+  static defaultProps = {
+    country: "in",
+    pageSize: 9,
+    category: "science",
     // search:""
-  }
-  
-  static propTypes ={
-    country:PropTypes.string,
-    pageSize:PropTypes.number,
-    category:PropTypes.string,
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
     // search:PropTypes.string,
-  }
-  captal =(string)=>{
-    return string.charAt(0).toUpperCase()+string.slice(1);
-  }
+  };
+  captal = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
   constructor(props) {
     super(props);
 
@@ -29,16 +29,16 @@ export class News extends Component {
       articles: [],
       loding: false,
       page: 1,
-      txt:'bitcoin'
+      totalResults :0,
     };
-    document.title=`${this.captal(this.props.category)}-Zeeshan News`
-    this.handleChange =this.handleChange.bind(this);
+    document.title = `${this.captal(this.props.category)}-Zeeshan News`;
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(event){
-    this.setState({txt:event.target.value})
+  handleChange(event) {
+    this.setState({ txt: event.target.value });
   }
-  handleSubmit =async () => {
+  handleSubmit = async () => {
     // alert('A name was submitted: ' + this.state.txt);
     // event.preventDefault();
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c37873bb96bf47fba7e806d996b70418`;
@@ -49,78 +49,77 @@ export class News extends Component {
       // page: this.state.page - 1,
       articles: parsedDeta.articles,
     });
-  }
-  async updateNews(){
-    const url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c37873bb96bf47fba7e806d996b70418&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-      this.setState({loding:true})
+  };
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c37873bb96bf47fba7e806d996b70418&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loding: true });
     let deta = await fetch(url);
     let parsedDeta = await deta.json();
     this.setState({
       articles: parsedDeta.articles,
       totalResults: parsedDeta.totalResults,
-      loding:false
+      loding: false,
+      
     });
   }
   async componentDidMount() {
     this.updateNews();
   }
-  handleNextClick = async () => {
-    
-    this.setState({page:this.state.page + 1})
-    this.updateNews();
-  };
-  handleprvClick = async () => {
+  
  
-    this.setState({page:this.state.page - 1})
-    this.updateNews();
+  fetchMoreData = async () => {
+    this.setState({page:this.state.page+1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c37873bb96bf47fba7e806d996b70418&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loding: true });
+    let deta = await fetch(url);
+    let parsedDeta = await deta.json();
+    this.setState({
+      articles:this.state.articles.concat(parsedDeta.articles),
+      totalResults: parsedDeta.totalResults,
+      loding: false,
+      
+    });
+    
   };
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center">ZeeshanNews - Top {this.captal(this.props.category)} Headlines</h1>
+        <h1 className="text-center">
+          ZeeshanNews - Top {this.captal(this.props.category)} Headlines
+        </h1>
+
+        {/* {this.state.loding &&<Spinner/>} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description}
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://gaadiwaadi.com/wp-content/uploads/2023/07/Ola-S1-Air.jpg"
+                    }
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          </div>
+        </InfiniteScroll>
         
-       
-            {this.state.loding &&<Spinner/>}
-        <div className="row">
-          {!this.state.loding &&  this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title : ""}
-                  description={element.description}
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://gaadiwaadi.com/wp-content/uploads/2023/07/Ola-S1-Air.jpg"
-                  }
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            type="button"
-            disabled={this.state.page <= 1}
-            className="btn btn-dark"
-            onClick={this.handleprvClick}
-          >
-            &larr; Previous
-          </button>
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.propspageSize)}
-          >
-            Next &rarr;
-          </button>
-        </div>
       </div>
     );
   }
