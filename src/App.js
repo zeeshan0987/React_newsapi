81d8fc1ef5ffcb901919fa425e0c1c1c26c16688
zeeshan0987 <zeeshan0987ali@gmail.com>
2023-08-10 11:45:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,112 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState } from 'react'
-import NavBar from './components/NavBar';
-import News from './components/News';
-import LoadingBar from 'react-top-loading-bar'
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
-const App =()=> {
-  const apikey=process.env.REACT_APP_NEWS_API
-  
-  
-  const [progress,setProgress]=useState(0)
-  
-   
-  
-    return (
-      <div>
-        <BrowserRouter>
-        <LoadingBar
-        color='#f11946'
-        height={3}
-        progress={progress}
-        
-      />
-        <NavBar/>
+const App = () => {
+  const apikey = process.env.REACT_APP_NEWS_API;
+
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <LoadingBar color="#f11946" height={3} progress={progress} />
+        <NavBar />
         <Routes>
-        <Route path="/" element={<News setProgress={setProgress} apikey={apikey} key="a" category="general" />}/>
-        <Route path="business" element={<News setProgress={setProgress}  apikey={apikey}key="b" category="business" />}/>
-        <Route path="entertainment" element={<News setProgress={setProgress} apikey={apikey} key="c" category="entertainment" />}/>
-        <Route path="general" element={<News setProgress={setProgress} apikey={apikey} key="e" category="general" />}/>
-        <Route path="health" element={<News setProgress={setProgress} apikey={apikey} key="f" category="health" />}/>
-        <Route path="science" element={<News setProgress={setProgress} apikey={apikey} key="g" category="science" />}/>
-        <Route path="sports" element={<News setProgress={setProgress} apikey={apikey} key='h' category="sports" />}/>
-        <Route path="technology" element={<News setProgress={setProgress} apikey={apikey} key='i' category="technology" />}/>
-        
+          <Route
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="a"
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="business"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="b"
+                category="business"
+              />
+            }
+          />
+          <Route
+            path="entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="c"
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            path="general"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="e"
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="health"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="f"
+                category="health"
+              />
+            }
+          />
+          <Route
+            path="science"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="g"
+                category="science"
+              />
+            }
+          />
+          <Route
+            path="sports"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="h"
+                category="sports"
+              />
+            }
+          />
+          <Route
+            path="technology"
+            element={
+              <News
+                setProgress={setProgress}
+                apikey={apikey}
+                key="i"
+                category="technology"
+              />
+            }
+          />
         </Routes>
-          
-        </BrowserRouter>
-      </div>
-    
-      
-    )
-  
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 export default App;
--- src/components/News.js
+++ src/components/News.js
@@ -5,20 +5,17 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
 export class News extends Component {
   static defaultProps = {
     country: "in",
     pageSize: 9,
     category: "science",
-    
   };
 
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-    
   };
   captal = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -30,7 +27,7 @@ export class News extends Component {
       articles: [],
       loding: true,
       page: 1,
-      totalResults :0,
+      totalResults: 0,
     };
     document.title = `${this.captal(this.props.category)}-Zeeshan News`;
     this.handleChange = this.handleChange.bind(this);
@@ -40,7 +37,6 @@ export class News extends Component {
     this.setState({ txt: event.target.value });
   }
   handleSubmit = async () => {
-    
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}`;
     let deta = await fetch(url);
     let parsedDeta = await deta.json();
@@ -61,69 +57,72 @@ export class News extends Component {
       articles: parsedDeta.articles,
       totalResults: parsedDeta.totalResults,
       loding: false,
-      
     });
     this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
   }
-  
- 
+
   fetchMoreData = async () => {
-    this.setState({page:this.state.page+1})
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c37873bb96bf47fba7e806d996b70418&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-    
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=c37873bb96bf47fba7e806d996b70418&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+
     let deta = await fetch(url);
     let parsedDeta = await deta.json();
     this.setState({
-      articles:this.state.articles.concat(parsedDeta.articles),
+      articles: this.state.articles.concat(parsedDeta.articles),
       totalResults: parsedDeta.totalResults,
       loding: false,
-      
     });
-    
   };
   render() {
     return (
       <>
-        <h1 className="text-center" style={{margin:"35px 0px",marginTop:'90px'}}>
+        <h1
+          className="text-center"
+          style={{ margin: "35px 0px", marginTop: "90px" }}
+        >
           ZeeshanNews - Top {this.captal(this.props.category)} Headlines
         </h1>
 
-        {this.state.loding &&<Spinner/>}
+        {this.state.loding && <Spinner />}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
           hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner/>}
+          loader={<Spinner />}
         >
           <div className="container">
-          <div className="row">
-            {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description}
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://gaadiwaadi.com/wp-content/uploads/2023/07/Ola-S1-Air.jpg"
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={element.description}
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://gaadiwaadi.com/wp-content/uploads/2023/07/Ola-S1-Air.jpg"
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          </div>
-          
         </InfiniteScroll>
-        
       </>
     );
   }
